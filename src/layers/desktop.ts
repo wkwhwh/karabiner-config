4b321b2c85527567d3905c23939245d390641965
Marc David <7226788+wkwhwh@users.noreply.github.com>
2024-05-26 17:13:14 +0100

    fix: add move_to_desktop script

--- src/layers/desktop.ts
+++ src/layers/desktop.ts
@@ -1,13 +1,14 @@
-import { toKey } from 'karabiner.ts';
-import { toDeeplink } from '../utils/to-links';
+import { to$, toKey } from 'karabiner.ts';
 
 export const desktop = {
   h: toKey('1', '⌃'),
   j: toKey('2', '⌃'),
   k: toKey('3', '⌃'),
   l: toKey('4', '⌃'),
-  n: toDeeplink('raycast://extensions/raycast/window-management/next-desktop'),
-  p: toDeeplink(
-    'raycast://extensions/raycast/window-management/previous-desktop',
-  ),
+  n: toKey('→', '⌃'),
+  p: toKey('←', '⌃'),
+  1: to$(`osascript ~/scripts/move_to_desktop.scpt "18"`),
+  2: to$(`osascript ~/scripts/move_to_desktop.scpt "19"`),
+  3: to$(`osascript ~/scripts/move_to_desktop.scpt "20"`),
+  4: to$(`osascript ~/scripts/move_to_desktop.scpt "21"`),
 };
